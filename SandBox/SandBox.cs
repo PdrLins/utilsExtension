8a1a2912b14e79a4063dc7f85f2adb4d47aa0e82
Pedro Lins <30423539+PdrLins@users.noreply.github.com>
2018-08-24 13:15:28 -0300

    Add files via upload

---
+++ SandBox/SandBox.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Xml;
+using UtilExtensions;
+
+namespace SeachXmlNode
+{
+    class SandBox
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var doc = new XmlDocument();
+            doc.SeachXmlNode("my node");
+
+        }
+
+
+    }
+}
---
+++ SandBox/UtilExtensions.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml;
+
+//System.Collections.Generic
+
+namespace UtilExtensions
+{
+    public static class UtilExtensions
+    {
+        public static XmlNode SeachXmlNode(this XmlNode node, String nodeName)
+        {
+            if (node.LocalName == nodeName)
+                return node;
+
+            if (node.ChildNodes.Count == 0)
+                return null;
+
+            foreach (XmlNode no in node.ChildNodes)
+            {
+                var nodeSought = SeachXmlNode(no, nodeName);
+
+                if (nodeSought == null)
+                    continue;
+                return nodeSought;
+            }
+
+            return null;
+        }
+
+        public static List<Dictionary<int, List<T>>> Split<T>(this IEnumerable<T> list, int? maxlength = 25)
+        {
+            if (list.Any())
+            {
+                var listAux = new List<T>();
+                var rList = new List<Dictionary<int, List<T>>>();
+                var seq = 1;
+                var page = 1;
+
+                foreach (var i in list)
+                {
+                    listAux.Add(i);
+                    if (seq == maxlength)
+                    {
+                        rList.Add(new Dictionary<int, List<T>>() { { page, listAux } });
+                        listAux = new List<T>();
+                        seq = 0;
+                        page++;
+                    }
+                    seq++;
+                }
+                rList.Add(new Dictionary<int, List<T>>() { { page, listAux } });
+                return rList;
+            }
+
+            return new List<Dictionary<int, List<T>>>();
+
+        }
+
+        public static List<Dictionary<int, List<T>>> Split<T>(this IGrouping<string, T> list, int? maxlength = 25)
+        {
+            if (list.Any())
+            {
+                var aList = new List<T>();
+                var rList = new List<Dictionary<int, List<T>>>();
+                var seq = 1;
+                var page = 1;
+
+                foreach (var i in list)
+                {
+                    aList.Add(i);
+                    if (seq == maxlength)
+                    {
+                        rList.Add(new Dictionary<int, List<T>>() { { page, aList } });
+                        aList = new List<T>();
+                        seq = 0;
+                        page++;
+                    }
+                    seq++;
+                }
+
+                rList.Add(new Dictionary<int, List<T>>() { { page, aList } });
+                return rList;
+            }
+
+            return new List<Dictionary<int, List<T>>>();
+        }
+    }
+}
